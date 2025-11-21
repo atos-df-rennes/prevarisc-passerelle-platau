d4f68a3b4ed497de63f34fa168508f318aa53d1d
Maxime Merrien <maxime.merrien@atos.net>
2025-11-21 10:04:57 +0100

    cs-fix

--- tests/Dto/DossierTest.php
+++ tests/Dto/DossierTest.php
@@ -2,14 +2,13 @@
 
 namespace App\Tests\Dto;
 
-use App\Dto\Dossier;
 use App\Dto\Information;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\Serializer\Serializer;
+use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Component\PropertyInfo\Extractor\PhpDocExtractor;
 use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
 use Symfony\Component\Serializer\Normalizer\ArrayDenormalizer;
-use Symfony\Component\Serializer\SerializerInterface;
 
 final class DossierTest extends TestCase
 {
@@ -19,7 +18,7 @@ final class DossierTest extends TestCase
     {
         parent::setUp();
 
-        $normalizers = [new ArrayDenormalizer(), new ObjectNormalizer(null, null, null, new PhpDocExtractor())];
+        $normalizers       = [new ArrayDenormalizer(), new ObjectNormalizer(null, null, null, new PhpDocExtractor())];
         $this->serializer  = new Serializer($normalizers);
     }
 
@@ -38,54 +37,54 @@ final class DossierTest extends TestCase
         $this->assertSame($expectedDemandeursNames, $dossier->getDemandeursAsString($demandeurs));
     }
 
-    public static function demandeurs(): \Generator
+    public static function demandeurs() : \Generator
     {
         yield 'demandeurs renseignés avec rôle valide' => [
             __DIR__.'/../fixtures/demandeurs.json',
             2,
-            'BROUARD Guillaume / CARABI CARABO Toto Test'
+            'BROUARD Guillaume / CARABI CARABO Toto Test',
         ];
 
         yield 'demandeurs non renseignés' => [
             __DIR__.'/../fixtures/demandeurs_vide.json',
             0,
-            null
+            null,
         ];
 
         yield 'demandeurs renseignés avec rôle non valide' => [
             __DIR__.'/../fixtures/demandeurs_role_invalide.json',
             1,
-            'CARABI CARABO Toto Test'
+            'CARABI CARABO Toto Test',
         ];
 
         yield 'demandeurs sans noms' => [
             __DIR__.'/../fixtures/demandeurs_sans_noms.json',
             2,
-            'BROUARD Guillaume / Toto Test'
+            'BROUARD Guillaume / Toto Test',
         ];
 
         yield 'demandeurs sans prénoms' => [
             __DIR__.'/../fixtures/demandeurs_sans_prenoms.json',
             2,
-            'BROUARD Guillaume / CARABI CARABO'
+            'BROUARD Guillaume / CARABI CARABO',
         ];
 
         yield 'demandeurs sans prénoms noms' => [
             __DIR__.'/../fixtures/demandeurs_sans_prenoms_noms.json',
             2,
-            'BROUARD Guillaume / Dénomination TOTO'
+            'BROUARD Guillaume / Dénomination TOTO',
         ];
 
         yield 'demandeurs sans prénoms noms dénomination' => [
             __DIR__.'/../fixtures/demandeurs_sans_prenoms_noms_denomination.json',
             2,
-            'BROUARD Guillaume / Raison sociale TOTO'
+            'BROUARD Guillaume / Raison sociale TOTO',
         ];
 
         yield 'demandeurs sans infos' => [
             __DIR__.'/../fixtures/demandeurs_sans_infos.json',
             2,
-            'BROUARD Guillaume / Inconnu'
+            'BROUARD Guillaume / Inconnu',
         ];
     }
 }
