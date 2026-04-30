4e19bf90941dc4bb876429915a8ee4811f2ec014
Maxime Merrien <maxime.merrien@atos.net>
2026-04-30 12:25:25 +0200

    debug: log des hashs des fichiers

--- src/Service/PlatauPiece.php
+++ src/Service/PlatauPiece.php
@@ -26,13 +26,16 @@ final class PlatauPiece extends PlatauAbstract
         \assert(\array_key_exists('VirtualFolderId', $file));
         $syncplicity_folder_id = (string) $file['VirtualFolderId'];
 
+        $hash_sha512 = hash('sha512', $file_contents);
+        error_log(\sprintf('[PLATAU_HASH_DEBUG] Fichier "%s" uploadé : taille=%d octets, sha256_syncplicity=%s, sha512_platau=%s', $filename, strlen($file_contents), hash('sha256', $file_contents), $hash_sha512));
+
         $document = [
             'fileId' => $syncplicity_file_id,
             'folderId' => $syncplicity_folder_id,
             'dtProduction' => (new \DateTime())->format('Y-m-d'),
             'idActeurProducteur' => (string) $this->getConfig()['PLATAU_ID_ACTEUR_APPELANT'],
             'algoHash' => 'SHA-512',
-            'hash' => hash('sha512', $file_contents),
+            'hash' => $hash_sha512,
             'nomTypeDocument' => $type_document,  // Nomenclature TYPE_DOCUMENT
             'nomTypeProducteurDoc' => 1,  // Nomenclature NATURE_PIECE. Toujours à 1 : "Personne jouant un rôle dans un dossier"
         ];
